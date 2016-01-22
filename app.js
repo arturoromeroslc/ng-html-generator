9998ace4dd0851b8ebe1b5a0d20be94f703be6fc
Arturo <arturo.romerotcs@gmail.com>
2016-01-22 00:56:36 -0700

    code complete for proof of concept

--- app.js
+++ app.js
@@ -7,49 +7,6 @@
         var vm = this,
             tableHtmlTag = document.getElementById('Table').children[0];
 
-        vm.addRow = function() {
-            var rowCount = tableHtmlTag.rows.length,
-                newRow = tableHtmlTag.insertRow(rowCount),
-                newCell = newRow.insertCell(0);
-
-            (rowCount % 2 === 0) ? addBlueStyle(newCell) : addGreyStyles(newCell);
-
-            addDefaultText(newCell);
-            addButton(rowCount);
-        };
-
-        vm.addCell = function(n) {
-            var row = tableHtmlTag.rows[n],
-                lastCell = row.cells[row.cells.length - 1],
-                newCell = row.insertCell(lastCell);
-
-            (n % 2 === 0) ? addBlueStyle(newCell) : console.log(n % 2);
-
-            addDefaultText(newCell);
-        };
-
-        vm.deleteCell = function(n) {
-            var row = tableHtmlTag.rows[n],
-                lastCell = row.cells[row.cells.length - 1],
-                newCell = row.deleteCell(lastCell);
-        };
-
-        vm.showHtml = function() {
-            var tableHtml = $('#Table');
-            tableHtml.find('*').removeAttr('class');
-            $('.pre-code').text(($(tableHtml).html()));
-        };
-
-        function addButton(n) {
-            var htmlButton = '<button type="button" ng-click="tableController.addCell(' + n + ')">Add Cell</button>',
-                compiledButton = $compile(htmlButton)($scope);
-            angular.element($('.buttons')).append(compiledButton);
-        }
-
-        function addDefaultText(newCell) {
-            newCell.appendChild(document.createTextNode('Cell'));
-        }
-
         function addBlueStyle(newCell) {
             $(newCell).css({
                 'background-color': '#02235c',
@@ -59,13 +16,70 @@
                 'font-weight': 'bold'
             }).attr("contentEditable", true);
         }
-
+        
         function addGreyStyles(newCell) {
             $(newCell).css({
+                'background-color': '#cccccc',
                 'font-weight': 'bold',
-                'font-size': '12px',
+                'font-size': '14px',
                 'color': '#000'
             }).attr("contentEditable", true);
         }
+
+        function addDefaultText(newCell) {
+            newCell.appendChild(document.createTextNode('Cell'));
+        }
+
+        function getCurrentRow(n) {
+            return tableHtmlTag.rows[n]
+        }
+
+        function getLastCell(row) {
+            return row.cells[row.cells.length - 1]
+        }
+
+        function appendButtons(rowNumber) {
+            var buttons = [],
+            htmlButtons = [
+                `<button class="inline-block" ng-click="tableController.addCell(${rowNumber})">Add Cell</button>`, 
+                `<button class="inline-block" ng-click="tableController.deleteCell(${rowNumber})">Delete Cell</button>`
+            ];
+            angular.forEach(htmlButtons, function(button, key) {
+                buttons.push($compile(button)($scope))
+            });
+
+            $('.buttons').append($("<div/>").append(buttons));
+        }
+
+        vm.newRowWithCell = function() {
+            var rowLength = tableHtmlTag.rows.length,
+                newCell = tableHtmlTag.insertRow(rowLength).insertCell(0);
+
+            (rowLength % 2 === 0) ? addBlueStyle(newCell) : addGreyStyles(newCell);
+            
+            addDefaultText(newCell);
+            appendButtons(rowLength);
+        };
+
+        vm.addCell = function(rowNumber) {
+            var currentRow = getCurrentRow(rowNumber),
+                newCell = currentRow.insertCell(getLastCell(currentRow));
+
+            (rowNumber % 2 === 0) ? addBlueStyle(newCell) : addGreyStyles(newCell);
+            addDefaultText(newCell);
+        };
+
+
+        vm.deleteCell = function(rowNumber) {
+            var currentRow = getCurrentRow(rowNumber),
+                lastCell = getLastCell(currentRow),
+                newCell = currentRow.deleteCell(lastCell);
+        };
+
+        vm.showHtml = function() {
+            var tableHtml = $('#Table');
+            tableHtml.find('*').removeAttr('class');
+            $('.pre-code').text(($(tableHtml).html()));
+        };
     }
 })();
